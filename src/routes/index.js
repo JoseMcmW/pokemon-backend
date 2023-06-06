9bdc4ebe2d4ef084d011eabf050fb0ac9088eb8c
Jose Centeno <josemanuelcm25@gmail.com>
2023-06-06 14:26:52 -0300

    Base de datos terminada y conectada

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+const pokemons = require('../controllers/pokemons');
 
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+/* router.get('/pokemons', pokemons); */
 
 
 module.exports = router;
